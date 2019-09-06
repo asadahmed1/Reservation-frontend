81d1e02367f5553570d8f65c79c5c6990232945d
asadahmed1 <asadahmed6345@gmail.com>
2019-09-06 13:14:34 -0700

    comments added

--- client/src/app/reservaation/reservaation.component.ts
+++ client/src/app/reservaation/reservaation.component.ts
@@ -7,144 +7,97 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReservaationComponent implements OnInit {
 
-  public reservation = Array();
+public reservation = Array();
  public name: string;
  public email: string;
- public rooms = ['Single Person', 'Two Person','Family'];
+ // drop dwon array
+ public rooms = ['Single Person', 'Two Person', 'Family'];
  public roomtype: string;
- public arrivalDate:string;
+ public arrivalDate: string;
  public guests: string;
  public pickup: string;
  public flightno: string;
- public _id:string
+ public _id: string;
  public request: string;
- public radio:String
- public rdop:string
-public index=-1
+ public radio: String;
+ // intialize id
+public index = -1;
 public id;
 
   constructor() { }
 
   ngOnInit() {
   }
-  // gettingIndex() {
+  // remove data from text  boxes after different operations performed(e.g update,delete,create)
+  Resetdata(){
+    this.name = '';
+    this.email = '';
+    this.roomtype = '';
+    this.arrivalDate = '';
+    this.guests = '';
+    this.pickup = '';
+    this.flightno = '';
+    this.request = '';
+    this.radio = '';
 
-  //   for (let i = 0; i < this.reservation.length; i++) {
-  //     this.index = i
-
-
-  //   }
-  // }
+  }
+  // add new data
   AddReservation() {
 
     for (let i = 0; i < this.reservation.length; i++) {
-      this.index = i}
+      this.index = i; }
 
     const newReservation = {
-      id:this.index,
+      id: this.index,
       name: this.name,
       email: this.email,
       roomtype: this.roomtype,
-      arrivalDate:this.arrivalDate,
+      arrivalDate: this.arrivalDate,
       guests: this.guests,
       pickup: this.pickup,
       flightno: this.flightno,
       request: this.request,
       radio:  this.radio
 
-    }
-    console.log(this.reservation)
-
-    this.name =''
-    this.email =''
-    this.roomtype =''
-    this.arrivalDate=''
-    this.guests =''
-    this.pickup =''
-    this.flightno =''
-    this.request =''
-    this.radio =''
-
+    };
 
     this.reservation.push(newReservation);
-// console.log(this.students)
+    this.Resetdata()
+//
   }
+  // delete one record
   deleteStudent(index) {
 
 
         this.reservation.splice(index, 1);
-
-      // if(this.students[i]._id==id){
-      //   this.students.splice(i,1);
-
-
-
-      // }
-
+        this.Resetdata()
   }
+  // for updating
   updateReservation(index) {
-    this.reservation[index].name=this.name;
-    this.reservation[index].email=this.email;
-    this.reservation[index].roomtype=this.roomtype;
-    this.reservation[index].arrivalDate=this.arrivalDate;
-    this.reservation[index].guests=this.guests;
-    this.reservation[index].pickup=this.pickup;
-    this.reservation[index].flightno=this.flightno;
-    this.reservation[index].request=this.request;
+    this.reservation[index].name = this.name;
+    this.reservation[index].email = this.email;
+    this.reservation[index].roomtype = this.roomtype;
+    this.reservation[index].arrivalDate = this.arrivalDate;
+    this.reservation[index].guests = this.guests;
+    this.reservation[index].pickup = this.pickup;
+    this.reservation[index].flightno = this.flightno;
+    this.reservation[index].request = this.request;
+    this.Resetdata()
+    return this.reservation;
 
-  //   const updatereservation = {
-  //     id:_id,
-  //     cont:{
-  //       name: this.name,
-  //     email: this.email,
-  //     roomtype: this.roomtype,
-  //     arrivalDate:this.arrivalDate,
-  //     guests: this.guests,
-  //     pickup: this.pickup,
-  //     flightno: this.flightno,
-  //     request: this.request,
-  //     radio:  this.radio
-  // }
-  //   }
-  //   this.reservation.splice(_id,1)
-  //   this.reservation.push(updatereservation)
-  //   console.log(updatereservation)
-    this.name =''
-    this.email =''
-    this.roomtype =''
-    this.arrivalDate=''
-    this.guests =''
-    this.pickup =''
-    this.flightno =''
-    this.request =''
-    this.radio=''
-
-    // this.reservation[index].cityname=this.cityname;
-    return this.reservation
-    // var update = {index:{
-    //     name: this.name,
-    //     Fname: this.Fname,
-    //     Addres: this.Address,
-    //     cityname: this.cityname
-    //   }
-    // }
-    // tslint:disable-next-line: semicolon
-    // console.log(update)
-    // this.students.update(update)
 
 
 
   }
+// for editing the data
+  editReservation(id: any) {
 
-  editReservation(id:any) {
 
-
-    for (let i = 0; i < this.reservation.length; i++) {
-      if(this.reservation[i].id==id){
+   for (let i = 0; i < this.reservation.length; i++) {
+      if (id == id){
 
 
       var stdnt = (this.reservation[i]);
-
 
       // console.log(this.stdnt)
       this.name = stdnt.name;
